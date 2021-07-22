932c71983b01f5d0bf06bc0d5897b9d2ee497e53
Brent <bgardner@squarelabs.net>
2021-07-22 11:05:36 -0600

    Can serve list of nodes

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-// https://zwave-js.github.io/node-zwave-js/#/getting-started/quickstart
 const { Driver } = require("zwave-js");
+const express = require('express')
+const app = express()
 
+const nodes = [];
+
+app.get('/nodes', async (req, res) => {
+    res.json(nodes);
+});
+
+// https://zwave-js.github.io/node-zwave-js/#/getting-started/quickstart
 (async () => {
-// Tell the driver which serial port to use
     const driver = new Driver("/dev/ttyACM0");
-// You must add a handler for the error event before starting the driver
     driver.on("error", (e) => {
-        // Do something with it
-        console.error(e);
+        console.error(e); // You must add a handler for the error event before starting the driver
     });
-// Listen for the driver ready event before doing anything with the driver
     driver.once("driver ready", () => {
-        /*
-        Now the controller interview is complete. This means we know which nodes
-        are included in the network, but they might not be ready yet.
-        The node interview will continue in the background.
-        */
-
         driver.controller.nodes.forEach((node) => {
             //console.log(node);
             // e.g. add event handlers to all known nodes
         });
 
-        // When a node is marked as ready, it is safe to control it
         for(let id of driver.controller.nodes.keys()) {
             const node = driver.controller.nodes.get(id);
             node.once("ready", async () => {
+                nodes.push({id});
                 // e.g. perform a BasicCC::Set with target value 50
                 // await node.commandClasses.Basic.set(50);
             });
         }
     });
-// Start the driver. To await this method, put this line into an async method
     await driver.start();
+})();
 
 // driver.destroy();
-})();
+
+app.listen(3001)
