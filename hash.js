18ea22f8a4654df948f795ef9527ce16013c6c02
Brent <bgardner@squarelabs.net>
2022-03-11 13:19:39 -0700

    Only index a directory once

--- hash.js
+++ hash.js
@@ -13,17 +13,29 @@ const imgHash = (path) => {
 }
 
 const hashes = {};
+let files = {};
+try {
+    files = JSON.parse(fs.readFileSync('files.json', 'utf-8'));
+} catch(e) {
+    
+}
 const scan = async (directoryName) => {
-    let files = await fsp.readdir(directoryName, {withFileTypes: true});
-    for (let f of files) {
+    let dir = await fsp.readdir(directoryName, {withFileTypes: true});
+    for (let f of dir) {
         try {
             const fullPath = path.join(directoryName, f.name);
+            if(files[fullPath]) {
+                continue;
+            }
             if (f.isDirectory()) {
+                fs.writeFileSync('hashes.json', JSON.stringify(hashes, null, 3));
+                fs.writeFileSync('files.json', JSON.stringify(files, null, 3));
                 await scan(fullPath)
             } else if(['.jpg', '.jpeg'].includes(path.extname(f.name).toLocaleLowerCase())) {
                 console.log(`Hashing ${fullPath}`);
                 const hash = await imgHash(fullPath);
-                
+
+                files[fullPath] = hash;
                 if(!hashes[hash]) hashes[hash] = [];
                 hashes[hash].push(fullPath);
                 if(hashes[hash].length > 1) {
@@ -35,10 +47,11 @@ const scan = async (directoryName) => {
             console.error(`Error on file ${f}`, ex);
         }
     }
+    fs.writeFileSync('hashes.json', JSON.stringify(hashes, null, 3));
+    fs.writeFileSync('files.json', JSON.stringify(files, null, 3));
 }
 
 const directoryName = '/media/bgardner/backup/Backup/Shared/Pictures/'; // TODO: move to config
 (async () => {
     await scan(directoryName);
-    fs.writeFileSync('hashes.json', JSON.stringify(hashes, null, 3));
 })();
