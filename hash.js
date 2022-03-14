7d25b3d8aca60fb451b022989e74a7350558a5cc
Brent <bgardner@squarelabs.net>
2022-03-14 14:29:24 -0600

    Hash recursively in place

--- hash.js
+++ hash.js
@@ -8,15 +8,22 @@ const { imageHash } = require('image-hash');
 // https://auth0.com/blog/read-edit-exif-metadata-in-photos-with-javascript/
 const getBase64DataFromJpegFile = filename => fs.readFileSync(filename).toString('binary');
 const getExifFromJpegFile = filename => piexif.load(getBase64DataFromJpegFile(filename));
-const changeExif = (file, hash) => {
-    const exif = getExifFromJpegFile(file);
+const changeExif = async (fullPath) => {
+    const exif = getExifFromJpegFile(fullPath);
+    if(exif.Exif[piexif.ExifIFD.ImageUniqueID]) {
+        console.log(`Skipping ${fullPath}`);
+        return;
+    }
+
+    console.log(`Hashing ${fullPath}`);
+    const hash = await imgHash(fullPath);
     exif.Exif[piexif.ExifIFD.ImageUniqueID] = hash;
 
-    const newImageData = getBase64DataFromJpegFile(file);
+    const newImageData = getBase64DataFromJpegFile(fullPath);
     const newExifBinary = piexif.dump(exif);
     const newPhotoData = piexif.insert(newExifBinary, newImageData);
     const fileBuffer = Buffer.from(newPhotoData, 'binary');
-    fs.writeFileSync(`${file}.jpg`, fileBuffer);
+    fs.writeFileSync(fullPath, fileBuffer);
 };
 
 const imgHash = (path) => {
@@ -34,10 +41,10 @@ const scan = async (directoryName) => {
         try {
             const fullPath = path.join(directoryName, f.name);
             const ext = path.extname(f.name).toLocaleLowerCase();
-            if (!f.isDirectory() && ['.jpg', '.jpeg'].includes(ext) && f.name.indexOf('arc') >= 0) {
-                console.log(`Hashing ${fullPath}`);
-                const hash = await imgHash(fullPath);
-                changeExif(fullPath, hash);
+            if (f.isDirectory()) {
+                await scan(fullPath);
+            } else if (['.jpg', '.jpeg'].includes(ext)) {
+                await changeExif(fullPath);
             }
         } catch(ex) {
             console.error(`Error on file ${f}`, ex);
@@ -45,7 +52,7 @@ const scan = async (directoryName) => {
     }
 }
 
-const directoryName = '/media/bgardner/backup1/Backup/Shared/Pictures/'; // TODO: move to config
+const directoryName = '/media/bgardner/backup2/Backup/Shared/Pictures/'; // TODO: move to config
 (async () => {
     await scan(directoryName);
 })();
