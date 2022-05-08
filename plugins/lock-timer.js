257415c2fa337cdac1029e5d2e484834f8cf5cf7
Brent <bgardner@squarelabs.net>
2022-05-08 11:31:57 -0600

    Error handling

--- plugins/lock-timer.js
+++ plugins/lock-timer.js
@@ -21,13 +21,17 @@ module.exports = async (driver, config, notify, db, opts) => {
             return; // Don't care
         }
         timeout = timeout || setTimeout(async () => {
-            const doorState = await frontDoor.getValue({ commandClass: 48, endpoint: 0, property: "Any" });
-            console.log(`doorState=`, doorState);
-            if(doorState === false) { // false = closed
-                await frontLock.setValue({commandClass, "property": "targetMode"}, 255);
-            } else {
-                await notify(`${opts.lockName} has been ${currentState} for ${opts.notifyThreshold} seconds`);
-            }    
+            try {
+                const doorState = await frontDoor.getValue({ commandClass: 48, endpoint: 0, property: "Any" });
+                console.log(`doorState=`, doorState);
+                if(doorState === false) { // false = closed
+                    await frontLock.setValue({commandClass, "property": "targetMode"}, 255);
+                } else {
+                    await notify(`${opts.lockName} has been ${currentState} for ${opts.notifyThreshold} seconds`);
+                }    
+            } catch(ex) {
+                console.error(`Error attempting to lock!`, ex);
+            }
             timeout = undefined;
         }, opts.notifyThreshold * 1000);
     };
